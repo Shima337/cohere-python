a472d261d152530f22b0ae7c4a8fc74b9932410c
Shima <andrey5093037@gmail.com>
2022-10-02 19:38:17 +0300

    to cohere

--- app.py
+++ app.py
@@ -31,11 +31,17 @@ def send_cohere():
 
 
 def trenings_prompt(data):
-    return f""" Compose a personal biography in narrative text:
-
-   Person biography: {data}
-
-    Narrative text min 150 words:
+    return f""" Person name: Maksim
+    Place of education: Belarussian Technical University
+    Education degree: Bachelor's degree in Economics
+    Place of work: Fetti
+    Position: Product director
+    Responsibilities: Creating product requirements documents, guiding other product leaders, and leading all product initiatives.
+    Achievements: Attracted 1,000 users to the new mobile app, expanding the geographical area of interaction.
+    Personal biography paragraph: I'm Maksim a Belarussian-born entrepreneur and technology executive who has co-founded and led several successful startups. I have a Bachelor's degree in Economics from Belarusian Technical University and extensive experience in building teams, organizing processes, and project management, including cross-team collaboration. I am passionate about using technology to solve problems and improve people's lives. My latest venture is Fetti, a mobile app for tourists and nomads. I consider myself a gifted leader and problem-solver who has a proven track record of success.
+    --
+    {data}
+    Personal biography paragraph:
     """
 
 @app.route('/dev/test')
