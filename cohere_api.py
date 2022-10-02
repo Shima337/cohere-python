d6677d21cf676970247b04d6e3285a75f8f2b0a2
Andrei <>
2022-10-02 14:13:46 +0300

    add custom params for cohere

--- cohere_api.py
+++ cohere_api.py
@@ -3,7 +3,21 @@ import cohere
 co = cohere.Client('j9fltyvpID3YyH82yoUqSCjhcydpMh1vG3lLS83z')
 
 def cohere_generate(data):
-    response = co.generate(prompt=data)
+    response = co.generate(prompt=data,
+                           model='large',
+                           preset=None,
+                           num_generations=3,
+                           max_tokens=250,
+                           temperature=0.7,
+                           k=0,
+                           p=0.75,
+                           frequency_penalty=0.0,
+                           presence_penalty=0.0,
+                           stop_sequences=None,
+                           return_likelihoods='NONE',
+                           truncate=None,
+                           logit_bias={}
+                           )
 
     for gen in response.generations:
         print('Prediction:\n\ttext = {}\n\tlikelihood = {}\n\ttoken_likelihoods = {}'.format(gen.text,
